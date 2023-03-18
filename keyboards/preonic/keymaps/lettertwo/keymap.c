a312261505dd6636a411f3cdf40961efb862f992
Eric Eldredge <lettertwo@gmail.com>
2023-03-18 16:13:01 -0400

    Preonic keymap

---
+++ keyboards/preonic/keymaps/lettertwo/keymap.c
@@ -0,0 +1,114 @@
+#include QMK_KEYBOARD_H
+
+enum preonic_layers {
+  _QWERTY,
+  _RAISE,
+  _LOWER,
+  _ADJUST
+};
+
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
+#define NEXT LGUI(KC_RBRC)
+#define PREV LGUI(KC_LBRC)
+
+#define SFT_ESC LSFT_T(KC_ESC)
+#define SFT_ENT RSFT_T(KC_ENT)
+#define CAP_WRD CAPS_WORD
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * |  ~   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |LS/ESC|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |RS/Ent|
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | LCtrl|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  '   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      | PREV | LGUI |LOWER |    SPACE    |RAISE | RLALT| NEXT |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTY] = LAYOUT_preonic_grid(
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_UNDS,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  SFT_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, SFT_ENT,
+  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+  XXXXXXX, XXXXXXX, PREV,    KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, NEXT,    XXXXXXX, XXXXXXX
+),
+
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  -   |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |   ~  |   (  |   )  |   ^  |   |  |      |      |  /   |   \  |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |CapWrd|   !  |   {  |   }  |   =  |   &  | Left | Down |  Up  |Right |   :  |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |LCrtl |   -  |   [  |   ]  |   +  |      |      |      |   <  |   >  |   ?  |  "   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      | LGUI |LOWER |      _      |RAISE | Ralt |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_LOWER] = LAYOUT_preonic_grid(
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
+  XXXXXXX, KC_TILD, KC_LPRN, KC_RPRN, KC_CIRC, KC_PIPE, XXXXXXX, XXXXXXX, KC_SLSH, KC_BSLS, XXXXXXX, XXXXXXX,
+  CAP_WRD, KC_EXLM, KC_LCBR, KC_RCBR, KC_EQL,  KC_AMPR, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_COLN, XXXXXXX,
+  _______, KC_MINS, KC_LBRC, KC_RBRC, KC_PLUS, XXXXXXX, XXXXXXX, XXXXXXX, KC_LT,   KC_GT,   KC_QUES, KC_DQUO,
+  XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, KC_UNDS, KC_UNDS, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
+),
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |  ~   |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  -   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |   ~  |   (  |   )  |   ^  |   |  |      |      |  /   |   \  |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |CapWrd|   !  |   {  |   }  |   =  |   &  | Left | Down |  Up  |Right |   :  |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |LCrtl |   -  |   [  |   ]  |   +  |      |      |      |   <  |   >  |   ?  |  "   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      | LGUI |LOWER |      _      |RAISE | Ralt |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = LAYOUT_preonic_grid(
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
+  XXXXXXX, KC_TILD, KC_LPRN, KC_RPRN, KC_CIRC, KC_PIPE, XXXXXXX, XXXXXXX, KC_SLSH, KC_BSLS, XXXXXXX, XXXXXXX,
+  CAP_WRD, KC_EXLM, KC_LCBR, KC_RCBR, KC_EQL,  KC_AMPR, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_COLN, XXXXXXX,
+  _______, KC_MINS, KC_LBRC, KC_RBRC, KC_PLUS, XXXXXXX, XXXXXXX, XXXXXXX,   KC_LT,   KC_GT, KC_QUES, KC_DQUO,
+  XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, KC_UNDS, KC_UNDS, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
+),
+
+
+/* Adjust (Lower + Raise)
+ * ,-----------------------------------------------------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |SLEEP |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | BOOT |      |      |      |      |      |      | VOLD | VOLU |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      | MUTE |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | BLITE|      |      | LGUI |LOWER |     PLAY    |RAISE | Ralt |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ADJUST] = LAYOUT_preonic_grid(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLEP,
+  QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  RGB_TOG, XXXXXXX, XXXXXXX, _______, _______, KC_MPLY, KC_MPLY, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
+)
+
+
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
