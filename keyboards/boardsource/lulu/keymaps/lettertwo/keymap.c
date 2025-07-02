7ac79d2c430005e11e83f0fd97ca2a0d1a67d5c5
Eric Eldredge <lettertwo@gmail.com>
2025-07-02 19:39:29 -0400

    Enable chordal hold and bootmagic

--- keyboards/boardsource/lulu/keymaps/lettertwo/keymap.c
+++ keyboards/boardsource/lulu/keymaps/lettertwo/keymap.c
@@ -112,3 +112,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
+const char PROGMEM chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
+  'L', 'L', 'L', 'L', 'L', 'L',           'R', 'R', 'R', 'R', 'R', 'R',
+  'L', 'L', 'L', 'L', 'L', 'L',           'R', 'R', 'R', 'R', 'R', 'R',
+  '*', 'L', 'L', 'L', 'L', 'L',           'R', 'R', 'R', 'R', 'R', '*',
+  '*', 'L', 'L', 'L', 'L', 'L', '*', '*', 'R', 'R', 'R', 'R', 'R', '*',
+                 '*', '*', '*', '*', '*', '*', '*', '*'
+);
